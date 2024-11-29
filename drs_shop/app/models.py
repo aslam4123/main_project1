36f92cd1af9a83cf2941c77d696ecc7b9284b359
aslam <thoppilaslam0987654321@gmail.com>
2024-11-29 17:27:42 +0530

    working

---
+++ drs_shop/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Product(models.Model):
+    pro_id=models.TextField()
+    name=models.TextField()
+    price=models.IntegerField()
+    ofr_price=models.IntegerField()
+    img=models.FileField()
+    dis=models.TextField()
