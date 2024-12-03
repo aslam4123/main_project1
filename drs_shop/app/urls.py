80c89d1d2436e00fe682cb91bcae757a165e741e
aslam <thoppilaslam0987654321@gmail.com>
2024-12-03 09:28:37 +0530

    working

--- drs_shop/app/urls.py
+++ drs_shop/app/urls.py
@@ -12,6 +12,7 @@ urlpatterns=[
     path('add_prod',views.add_prod),
     path('edit_prod/<pid>',views.edit_prod),
     path('delete_prod/<pid>',views.delete_prod),
+    path('bookings',views.bookings),
 
 
     # --------------user---------------------
@@ -20,6 +21,12 @@ urlpatterns=[
     path('register',views.register),
     path('user_home',views.user_home),
     path('view_pro/<pid>',views.view_pro),
+    path('add_to_cart/<pid>',views.add_to_cart),
+    path('view_cart',views.view_cart),
+    path('delete_cart/<id>',views.delete_cart),
+    path('user_buy/<cid>',views.user_buy),
+    path('user_buy1/<pid>',views.user_buy1),
+    path('user_bookings',views.user_bookings)
     
 
 ]
